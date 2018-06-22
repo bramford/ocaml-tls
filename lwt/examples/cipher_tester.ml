5058cae5cbf6829047272353e3a1502261fe26a4
Bramford Horton <bram.horton@gmail.com>
2018-06-22 22:54:14 +1200

    fixup! Add cipher tester client

--- lwt/examples/cipher_tester.ml
+++ lwt/examples/cipher_tester.ml
@@ -12,7 +12,7 @@ let connect host port ca_dir cipher =
     in
     let%lwt () =
       Lwt_io.eprintf
-        "%sCIPHER SUCCESS: %s\n%!"
+        "%s          SUCCESS: %s\n%!"
         (if Tls.Ciphersuite.ciphersuite_tls12_only cipher then "TLS1.2 " else "       ")
         (Tls.Ciphersuite.ciphersuite_to_string cipher)
     in
@@ -20,7 +20,7 @@ let connect host port ca_dir cipher =
   with
   | Tls_lwt.Tls_alert Tls.Packet.HANDSHAKE_FAILURE ->
       Lwt_io.eprintf
-        "%sCIPHER FAILURE: %s\n%!"
+        "%sHANDSHAKE FAILURE: %s\n%!"
         (if Tls.Ciphersuite.ciphersuite_tls12_only cipher then "TLS1.2 " else "       ")
         (Tls.Ciphersuite.ciphersuite_to_string cipher)
   | Tls_lwt.Tls_alert alert as exn ->
@@ -32,14 +32,42 @@ let connect host port ca_dir cipher =
       (Tls.Engine.string_of_failure fail)
       ; raise exn
 
-let () =
-  let ciphers = Tls.Config.Ciphers.supported in
-  let host = "remoteok.io" in
-  let port = 443 in
-  let ca_dir = "/etc/ssl/certs" in
+let run_test host port ca_dir cipher_suite =
   let tests =
     Lwt_list.iter_p
       (fun cipher -> connect host port ca_dir cipher)
-      ciphers
+      cipher_suite
   in
   Lwt_main.run tests
+
+let () =
+  let host = ref "" in
+  let port = ref 443 in
+  let ca_dir = ref "/etc/ssl/certs" in
+  let ciphers = ref "default" in
+
+  let speclist = [
+    ("-port", Arg.Int (fun p -> port := p), "TCP port of remote server");
+    ("-ca-dir", Arg.String (fun d -> ca_dir := d), "Directory containing CA cert files");
+    ("-ciphers", Arg.String (fun c -> ciphers := c), "Cipher suite to use ('default' or 'supported')");
+  ] in
+
+  let cipher_suite =
+    match !ciphers with
+    | "default" -> Tls.Config.Ciphers.default
+    | "supported" -> Tls.Config.Ciphers.supported
+    | arg -> Printf.eprintf "ERROR: Invalid cipher suite '%s' (try 'default' or 'supported')" arg ; exit 1;
+  in
+
+  let usage = "Test TLS to a remote host\nUsage: cipher_tester HOSTNAME\n" in
+
+  Arg.parse
+    speclist
+    (fun anon_arg -> host := anon_arg )
+    usage;
+
+  match !host with
+  | "" -> Printf.eprintf "ERROR: No hostname given\n%s" usage; exit 1;
+  | _ -> ();
+
+  run_test !host !port !ca_dir cipher_suite
